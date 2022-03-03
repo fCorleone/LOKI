3482802adbd0953e2c0c7408ac443f2c702be252
fCorleone <mafc19@mails.tsinghua.edu.cn>
2022-03-03 10:30:27 +0800

    reformat the code and change the ci config file.

--- core/src/mutator.rs
+++ core/src/mutator.rs
@@ -52,54 +52,27 @@ mod tests {
     #[test]
     fn test_generate_random_number() {
         let n1: bool = generate_random_number().unwrap();
-        assert_eq!(
-            (n1 == true || n1 == false), 
-            true
-        );
+        assert_eq!((n1 == true || n1 == false), true);
         let n2: u32 = generate_random_number().unwrap();
-        assert_eq!(
-            (n2 >= u32::MIN && n2 < u32::MAX),
-            true
-        );
+        assert_eq!((n2 >= u32::MIN && n2 < u32::MAX), true);
         let n3: i32 = generate_random_number().unwrap();
-        assert_eq!(
-            (n3 >= i32::MIN && n3 < i32::MAX),
-            true
-        );
+        assert_eq!((n3 >= i32::MIN && n3 < i32::MAX), true);
         let n4: u64 = generate_random_number().unwrap();
-        assert_eq!(
-            (n4 >= u64::MIN && n4 < u64::MAX),
-            true
-        );
+        assert_eq!((n4 >= u64::MIN && n4 < u64::MAX), true);
         let n5: i64 = generate_random_number().unwrap();
-        assert_eq!(
-            (n5 >= i64::MIN && n5 < i64::MAX),
-            true
-        );
-        let p1: f32 = 10f32.powf(-(4 as f32)); 
+        assert_eq!((n5 >= i64::MIN && n5 < i64::MAX), true);
+        let p1: f32 = 10f32.powf(-(4 as f32));
         let n6: f32 = generate_random_number().unwrap();
-        assert_eq!(
-            (n6 >= p1 && n6 < (f32::MAX + p1)),
-            true
-        );
-        let p2: f64 = 10f64.powf(-(4 as f64)); 
+        assert_eq!((n6 >= p1 && n6 < (f32::MAX + p1)), true);
+        let p2: f64 = 10f64.powf(-(4 as f64));
         let n7: f64 = generate_random_number().unwrap();
-        assert_eq!(
-            (n7 >= p2 && n7 < (f64::MAX + p2)),
-            true
-        );
+        assert_eq!((n7 >= p2 && n7 < (f64::MAX + p2)), true);
     }
 
     #[test]
     fn test_get_random_bigger_u8() {
-        assert_eq!(
-            get_random_bigger_u8(10).unwrap() >= 10,
-            true
-        );
-        assert_eq!(
-            get_random_bigger_u8(u8::MAX).unwrap(),
-            u8::MAX
-        );
+        assert_eq!(get_random_bigger_u8(10).unwrap() >= 10, true);
+        assert_eq!(get_random_bigger_u8(u8::MAX).unwrap(), u8::MAX);
     }
 }
 
