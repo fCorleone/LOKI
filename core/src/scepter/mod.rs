3ba4c8c4554a40e48a91557c17d198b269479472
fCorleone <mafc19@mails.tsinghua.edu.cn>
2022-04-27 20:44:22 +0800

    add loki spec as a submodule

--- core/src/scepter/mod.rs
+++
@@ -1,4 +0,0 @@
-//! Scepter is the specification file used by LOKI
-
-/// indicates all the message types of a blockchain
-pub const MESSAGE_TYPE: [&'static str; 4] = ["PREPREPARE", "PREPARE", "SIGN", "VIEWCHANGE"];
